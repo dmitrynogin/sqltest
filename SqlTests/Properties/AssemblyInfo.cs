03cf6520fcdc549ddfb734889220f50c30998446
Dmitry Nogin <dmitryn@mail.com>
2018-05-02 10:13:20 -0700

    Define test

---
+++ SqlTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SqlTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SqlTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ea7200bb-0801-4f30-a926-468855e74199")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SqlTests/UnitTest1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Transactions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SqlTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        string CS { get; } = @"Data Source = (localdb)\MSSQLLocalDB; Initial Catalog = Test; Integrated Security = True; Connect Timeout = 30;";
+        string Update { get; } = "UPDATE [People] SET [Name] = 'Tom' WHERE [Id] = 1";
+        string Select { get; } = "SELECT [Name] FROM [People] WHERE [Id] = 1";
+
+
+        [TestMethod]
+        public void ShouldRead()
+        {
+            using (var scope = new TransactionScope(
+                TransactionScopeOption.Required,
+                new TransactionOptions
+                {
+                    IsolationLevel = IsolationLevel.ReadCommitted
+                }))
+            {
+                using (var c1 = new SqlConnection(CS))
+                {
+                    c1.Open();
+                    var cmd = c1.CreateCommand();
+                    cmd.CommandText = Update;
+                    cmd.CommandType = System.Data.CommandType.Text;
+                    cmd.ExecuteNonQuery();
+                }
+
+                using (var c2 = new SqlConnection(CS))
+                {
+                    c2.Open();
+                    var cmd2 = c2.CreateCommand();
+                    cmd2.CommandText = Select;
+                    cmd2.CommandType = System.Data.CommandType.Text;
+                    var name = cmd2.ExecuteScalar();
+
+                    Assert.AreEqual("Tom", name);
+                }
+            }
+        }
+    }
+}
