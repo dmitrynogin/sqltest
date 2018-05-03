edfdadfb4b273966736cb32e226973c7452e900b
Dmitry Nogin <dmitryn@mail.com>
2018-05-03 01:17:25 -0700

    Check EF 6.1.0

--- SqlTests/UnitTest1.cs
+++ SqlTests/UnitTest1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data.Entity;
 using System.Data.SqlClient;
 using System.Transactions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -8,13 +9,12 @@ namespace SqlTests
     [TestClass]
     public class UnitTest1
     {
-        string CS { get; } = @"Data Source = (localdb)\MSSQLLocalDB; Initial Catalog = Test; Integrated Security = True; Connect Timeout = 30;";
-        string Update { get; } = "UPDATE [People] SET [Name] = 'Tom' WHERE [Id] = 1";
-        string Select { get; } = "SELECT [Name] FROM [People] WHERE [Id] = 1";
-
-
+        const string CS = @"Data Source = (localdb)\MSSQLLocalDB; Initial Catalog = Test; Integrated Security = True; Connect Timeout = 30;";
+        const string Update = "UPDATE [People] SET [Name] = 'Tom' WHERE [Id] = 1";
+        const string Select = "SELECT [Name] FROM [People] WHERE [Id] = 1";
+        
         [TestMethod]
-        public void ShouldRead()
+        public void Should_See_Uncommitted_Updates()
         {
             using (var scope = new TransactionScope(
                 TransactionScopeOption.Required,
@@ -43,6 +43,51 @@ namespace SqlTests
                     Assert.AreEqual("Tom", name);
                 }
             }
+        }
+
+        [TestMethod]
+        public void Should_See_Uncommitted_EF_Updates()
+        {
+            using (var scope = new TransactionScope(
+                TransactionScopeOption.Required,
+                new TransactionOptions
+                {
+                    IsolationLevel = IsolationLevel.ReadCommitted
+                }))
+            {
+                var ctx = new TestContext();
+                var p = ctx.People.Find(1);
+                p.Name = "Tom";
+                ctx.SaveChanges();
+
+                using (var c2 = new SqlConnection(CS))
+                {
+                    c2.Open();
+                    var cmd2 = c2.CreateCommand();
+                    cmd2.CommandText = Select;
+                    cmd2.CommandType = System.Data.CommandType.Text;
+                    var name = cmd2.ExecuteScalar();
+
+                    Assert.AreEqual("Tom", name);
+                }
+            }
+        }
+
+
+        public class TestContext : DbContext
+        {
+            public TestContext()
+                : base(CS)
+            {
+            }
+
+            public DbSet<People> People { get; set; }
+        }
+
+        public class People
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
         }
     }
 }
